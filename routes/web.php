77a0960851cde8493b9b0c453d8838d3e9b40a64
shintaro <ghsprskob97@yahoo.co.jp>
2020-06-11 01:33:00 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+// Route::get('/', 'TasksController@index');
 
-Route::resource('tasks', 'TasksController');
+// Route::resource('tasks', 'TasksController');
+Route::get('/', function () {
+    return view('tasks.welcome');
+});
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
